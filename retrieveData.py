0c3943aebfc1180964343e969f296e7dcd02631d
SoccerStar24x7 <arnavhegde24@gmail.com>
2026-03-13 21:57:04 -0400

    added more

--- retrieveData.py
+++ retrieveData.py
@@ -4,9 +4,13 @@ def retrieveData(stock):
     ticker = yf.Ticker(stock)
 
     data = ticker.history(period="1mo")
-    data = data.drop(columns=["Dividends", "Stock Splits"])
+    data = data.drop(columns=["Dividends", "Stock Splits", "Capital Gains"])
+    data = data.iloc(0)
 
     exportCSV(data)
-    
+
 def exportCSV(data):
     data.to_csv('data.csv', index=False)
+
+if __name__ == "__main__":
+    retrieveData("SPMO")
--- test.cpp
+++ test.cpp
@@ -1,4 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-using namespace yfinance;
+int main() {
+    std::ifstream file("data.csv");
+    std::string line;
 
-Symbol SPMO = new Symbol("SPMO");
+    std::vector<std::string> open;
+    std::vector<std::string> high;
+    std::vector<std::string> low;
+    std::vector<std::string> close;
+    std::vector<std::string> volume;
+
+    while (std::getline(file, line)) {
+        std::string cell;
+        std::stringstream ss(line);
+        std::vector<std::string> row;
+
+        while (std::getline(ss, cell, ',')) {
+            row.push_back(cell);
+            std::cout << cell << " ";
+
+            for (int i = 1; i <= 5; i++) {
+                switch(i) {
+                    case 1:
+                        open.push_back(cell);
+                        break;
+
+                    case 2:
+                        high.push_back(cell);
+                        break;
+                    
+                    case 3:
+                        low.push_back(cell);
+                        break;
+
+                    case 4:
+                        close.push_back(cell);
+                        break;
+
+                    case 5:
+                        volume.push_back(cell);
+                        break;
+                };
+            }
+        }
+    }
+    for (int i = 0; i < volume.size(); i++)
+        std::cout << volume[i] << std::endl;
+}
